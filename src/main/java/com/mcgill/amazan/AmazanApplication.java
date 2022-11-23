de57e58269d6e18100fa5b6012e9547454f9b8b2
maxensdestine <maxensdestine@gmail.com>
2022-11-23 14:52:33 -0500

    Adding initial project files

---
+++ src/main/java/com/mcgill/amazan/AmazanApplication.java
@@ -0,0 +1,13 @@
+package com.mcgill.amazan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmazanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AmazanApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mcgill/amazan/AmazanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mcgill.amazan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmazanApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
