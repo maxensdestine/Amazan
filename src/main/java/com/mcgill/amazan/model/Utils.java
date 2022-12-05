e3265bfbc48fa8f1f65410655ae532110caf65e8
MaxensDestine <maxensdestine@gmail.com>
2022-12-05 12:19:41 -0500

    Revert "Model dev maxens"

--- src/main/java/com/mcgill/amazan/model/Utils.java
+++
@@ -1,8 +0,0 @@
-package com.mcgill.amazan.model;
-
-public class Utils {
-
-    public static boolean isNotNullOrEmpty(String string){
-        return string != null && string.length() > 0;
-    }
-}
