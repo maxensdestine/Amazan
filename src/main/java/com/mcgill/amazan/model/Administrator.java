18ac4b457d69c5297b86273786a3552e5ce29f2f
maxensdestine <maxensdestine@gmail.com>
2022-12-05 21:06:50 -0500

    Added an empty constructor to the administrator class to facilitate testing

--- src/main/java/com/mcgill/amazan/model/Administrator.java
+++ src/main/java/com/mcgill/amazan/model/Administrator.java
@@ -3,6 +3,8 @@ package com.mcgill.amazan.model;
 public class Administrator extends User
 {
 
+  public Administrator(){}
+  
   public Administrator(String firstName, String lastName, String email, String username, String passwordHash, boolean isDeleted, boolean isBanned)
   {
     super(firstName, lastName, email, username, passwordHash, isDeleted, isBanned);
